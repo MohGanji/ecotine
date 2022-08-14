a0c29b46087b3a486ff25bb62af2d98d1f1f7d0e
Mohammad macbook <m_ganji@sfu.ca>
2022-08-14 15:25:46 -0700

    refactor redirect script, add some more blogs

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const blogs = {
-    'dereksivers': 'https://sive.rs',
-    'timurban': 'https://waitbutwhy.com',
-    'austinkleon': 'https://austinkleon.com',
-    'sethgodin': 'https://seths.blog',
-    'lyndabarry': 'https://thenearsightedmonkey.tumblr.com',
-    'dereksiversbooks': 'https://sive.rs/books',
-    'markmanson': 'https://markmanson.net',
-    'ryanholiday': 'https://ryanholiday.net',
-    'stevenpressfield': 'https://stevenpressfield.com',
-    // 'annieduke': 'https://www.annieduke.com/', something goes wrong when Proxy.
-    'neilstrauss': 'https://www.neilstrauss.com',
-    'timferris': 'https://tim.blog',
-    'ericbarker': 'https://bakadesuyo.com',
-
+var blogs = {
+    'dereksivers': () => fetchPostFullArchive('https://sive.rs/blog', 'li > a', 'https://sive.rs'),
+    'timurban': () => fetchPostWithRandomUrl('https://waitbutwhy.com/random/'),
+    'austinkleon': () => fetchPostWithPagination('https://austinkleon.com/page', '.entry-title-link', 100),
+    'sethgodin': () => fetchPostWithPagination('https://seths.blog/page', 'div.post > h2 > a', 100),
+    'lyndabarry': () => fetchPostWithRandomUrl('https://thenearsightedmonkey.tumblr.com/random'),
+    'dereksiversbooks': () => fetchPostFullArchive('https://sive.rs/books', 'div > h2 > a', 'https://sive.rs'),
+    'markmanson': () => fetchPostFullArchive('https://markmanson.net/archive', '.archive-table td > a', 'https://markmanson.net'),
+    'ryanholiday': () => fetchPostFullArchive('https://ryanholiday.net/archive/', '#smart-archives-list li > a'),
+    'stevenpressfield': () => fetchPostWithPagination('https://stevenpressfield.com/blog/page', 'h2 > a', 100),
+    // 'annieduke': () => fetchPostWithPagination('https://www.annieduke.com/tag/article/page', 'a.elementor-post__read-more', 5), // Something goes wrong when proxy
+    'neilstrauss': () => fetchPostWithPagination('https://www.neilstrauss.com/blog/page', 'a.more-link', 50),
+    'timferris': () => fetchPostWithPagination('https://tim.blog/page', 'h3.entry-title > a', 100),
+    'ericbarker': () => fetchPostWithPagination('https://bakadesuyo.com/blog/page', 'h4 > a', 7),
+    'walkingtheworld': () => fetchPostFullArchive('https://walkingtheworld.substack.com/archive', 'a.post-preview-title'),
+    'tannergreer': () => fetchPostFullArchive('https://scholars-stage.org/scholars-stage-read-more/', 'li > a'),
+    'smtm': () => fetchPostFullArchive('https://slimemoldtimemold.com/archives/', 'li > a'),
+    'paco': () => fetchPostFullArchive('https://thehellyeahgroup.com/archive', 'a.archive-item-link', 'https://thehellyeahgroup.com'),
 }
-const PROXY = 'http://141.95.19.7:8888/'
+var PROXY = 'http://141.95.19.7:8888/'
 
 function select() {
     if(document.getElementById('btn').classList.contains('disabled')) 
@@ -23,243 +26,67 @@ function select() {
     
     const DEBUG_BLOG = null
     const blogKey = DEBUG_BLOG || _selectBlog()
-    _selectPostFromBlog(blogKey)
+    return blogs[blogKey]()
 
     // utility functions for select:
     function _selectBlog () {
-        let keys = Object.keys(blogs)
-        let randomKey = keys[Math.floor(Math.random() * keys.length)]
+        const keys = Object.keys(blogs)
+        const randomKey = keys[Math.floor(Math.random() * keys.length)]
         return randomKey
     }
+}
+
+function fetchPostWithPagination(blogUrl, selector, maxPage, prefixUrl = '') {
+    const page = Math.floor(Math.random() * maxPage)
+    const blogPageUrl = `${blogUrl}/${page}`
+    fetchPostFullArchive(blogPageUrl, selector, prefixUrl)
+}
+
+function fetchPostWithRandomUrl(randomUrl) {
+    _redirectTo(randomUrl)
+}
+
+function fetchPostFullArchive(blogUrl, selector, prefixUrl = '') {
+    fetch(`${PROXY}${blogUrl}`, {
+        method: 'GET'
+    })
+    .then(response => response.text())
+    .then(data => {
+        // create an html to parse the data.
+        var el = document.createElement('html');
+        el.innerHTML = data;
+
+        const posts = el.querySelectorAll(selector)
+        postSuffix = posts.item(randUpTo(posts.length)).getAttribute('href')
+        
+        url = prefixUrl + postSuffix
+        _redirectTo(url)
+    })
+    .catch(error => _handleError(error, blogUrl));
+}
+
+function _redirectTo(url) {
+    // console.log('url: ', url)
+    window.location.href = url;
+    setTimeout(() => {
+        _enableButton()
+    }, 1500)
     
-    function _selectPostFromBlog(blogKey) {
-        let url = 'http://m.ganji.blog'
-        
-        if(blogKey === 'dereksivers') {
-            fetch(`${PROXY}https://sive.rs/blog`, {
-                method: 'GET'
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
+}
 
-                const posts = el.querySelectorAll('li > a')
-                postSuffix = posts.item(randUpTo(posts.length)).getAttribute('href')
-                
-                url = blogs.dereksivers + postSuffix
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        }  else if(blogKey === 'timurban') {
-            url = 'https://waitbutwhy.com/random/'
-            _redirectTo(url)
+function _disableButton() {
+    document.getElementById('btn').classList.add('disabled')
+    document.getElementById('btn-text').innerText = "Wait..."
+}
+function _enableButton() {
+    document.getElementById('btn').classList.remove('disabled')
+    document.getElementById('btn-text').innerText = "Take Me Somewhere"
+}
 
-        } else if(blogKey === 'austinkleon') {
-            const page = Math.floor(Math.random() * 100)
-
-            fetch(`${PROXY}https://austinkleon.com/page/${page}`, {
-                "body": null,
-                "method": "GET"
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                const posts = el.querySelectorAll('.entry-title-link')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        } else if (blogKey === 'sethgodin') {
-            const page = Math.floor(Math.random() * 100)
-              
-            fetch(`${PROXY}https://seths.blog/page/${page}`, {
-                method: 'POST',
-                redirect: 'follow',
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                
-                const posts = el.querySelectorAll('div.post > h2 > a')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-
-            })
-            .catch(error => _handleError(error, blogKey));
-        } else if(blogKey === 'lyndabarry') {
-            url = 'https://thenearsightedmonkey.tumblr.com/random'
-            _redirectTo(url)
-        } else if(blogKey === 'dereksiversbooks') {
-            fetch(`${PROXY}https://sive.rs/books`, {
-                method: 'GET'
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-
-                const posts = el.querySelectorAll('div > h2 > a')
-                postSuffix = posts.item(randUpTo(posts.length)).getAttribute('href')
-                
-                url = blogs.dereksivers + postSuffix
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        } else if(blogKey === 'markmanson') {
-            fetch(`${PROXY}https://markmanson.net/archive`, {
-                method: 'GET'
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-
-                const posts = el.querySelectorAll('.archive-table td > a')
-                postSuffix = posts.item(randUpTo(posts.length)).getAttribute('href')
-                
-                url = blogs.markmanson + postSuffix
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        } else if(blogKey === 'ryanholiday') {
-            fetch(`${PROXY}https://ryanholiday.net/archive/`, {
-                method: 'GET'
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-
-                const posts = el.querySelectorAll('#smart-archives-list li > a')
-                const postUrl = posts.item(randUpTo(posts.length)).getAttribute('href')
-                
-                _redirectTo(postUrl)
-            })
-            .catch(error => _handleError(error, blogKey));
-        } else if(blogKey === 'stevenpressfield') {
-            const page = Math.floor(Math.random() * 100)
-
-            fetch(`${PROXY}https://stevenpressfield.com/blog/page/${page}`, {
-                "body": null,
-                "method": "GET"
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                const posts = el.querySelectorAll('h2 > a')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        } else if(blogKey === 'annieduke') {
-            const page = Math.floor(Math.random() * 5)
-            fetch(`${PROXY}https://www.annieduke.com/tag/article/page/${page}`, {
-                "body": null,
-                "method": "GET"
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                const posts = el.querySelectorAll('a.elementor-post__read-more')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        } else if(blogKey === 'neilstrauss') {
-            const page = Math.floor(Math.random() * 50)
-            fetch(`${PROXY}https://www.neilstrauss.com/blog/page/${page}`, {
-                "body": null,
-                "method": "GET"
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                const posts = el.querySelectorAll('a.more-link')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        } else if(blogKey === 'timferris') {
-            const page = Math.floor(Math.random() * 100)
-            fetch(`${PROXY}https://tim.blog/page/${page}`, {
-                "body": null,
-                "method": "GET"
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                const posts = el.querySelectorAll('h3.entry-title > a')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        } else if(blogKey === 'ericbarker') {
-            const page = Math.floor(Math.random() * 7)
-            fetch(`${PROXY}https://bakadesuyo.com/blog/page/${page}`, {
-                "body": null,
-                "method": "GET"
-            })
-            .then(response => response.text())
-            .then(data => {
-                // create an html to parse the data.
-                var el = document.createElement('html');
-                el.innerHTML = data;
-                const posts = el.querySelectorAll('h4 > a')
-                url = posts.item(randUpTo(posts.length)).getAttribute('href')
-                _redirectTo(url)
-            })
-            .catch(error => _handleError(error, blogKey));
-        
-        } 
-        return
-    }
-
-    function _redirectTo(url) {
-        // console.log('url: ', url)
-        window.location.href = url;
-        setTimeout(() => {
-            _enableButton()
-        }, 1500)
-        
-    }
-
-    function _disableButton() {
-        document.getElementById('btn').classList.add('disabled')
-        document.getElementById('btn-text').innerText = "Wait..."
-    }
-    function _enableButton() {
-        document.getElementById('btn').classList.remove('disabled')
-        document.getElementById('btn-text').innerText = "Take Me Somewhere"
-    }
-
-    function _handleError(error, name) {
-        console.log(`${name}:error`, error)
-        document.getElementById('btn').classList.remove('disabled')
-        select(); // retry
-    }
+function _handleError(error, name) {
+    console.log(`${name}:error`, error)
+    document.getElementById('btn').classList.remove('disabled')
+    select(); // retry
 }
 
 function randUpTo(num) {
